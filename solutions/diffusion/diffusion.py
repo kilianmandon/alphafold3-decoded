Fixed bug in bst lookup table computation.

solutions/diffusion/diffusion.py
@@ -1,7 +1,6 @@
 import numpy as np
 from torch import nn
 import torch
-import tqdm
 
 from config import Config
 from input_embedding.atom_attention import AtomAttentionDecoder, AtomAttentionEncoder
@@ -90,6 +89,8 @@ class DiffusionConditioning(nn.Module):
         self.fourier_w = nn.Parameter(torch.randn((c_fourier,)), requires_grad=False)
         self.fourier_b = nn.Parameter(torch.randn((c_fourier,)), requires_grad=False)
 
+        self.apply_af3_identical_layernorm = False
+
     def fourier_embedding(self, t_hat):
         # t_hat has shape (**batch_shape,)
         # out should have shape (**batch_shape, 1, c_fourier)
@@ -110,8 +111,10 @@ class DiffusionConditioning(nn.Module):
         s = torch.cat((s_trunk, s_input), dim=-1)
         tf_mask = torch.ones(s.shape[-1], device=s.device, dtype=bool)
         tf_mask[415] = tf_mask[447] = False
-        # s = self.linear_s(apply_layernorm_masked(s, self.layer_norm_s, tf_mask))
-        s = self.linear_s(self.layer_norm_s(s))
+        if self.apply_af3_identical_layernorm:
+            s = self.linear_s(apply_layernorm_masked(s, self.layer_norm_s, tf_mask))
+        else:
+            s = self.linear_s(self.layer_norm_s(s))
         n = self.fourier_embedding(t_hat)
         s = s + self.linear_fourier(self.layer_norm_fourier(n))
 

solutions/internal_tests.py
@@ -48,6 +48,7 @@ def main(test_name):
     config.diffusion_config.denoising_steps = 4
 
     model = Model(config)
+    model.diffusion_module.diffusion_conditioning.apply_af3_identical_layernorm = True
     params = torch.load('data/params/af3_pytorch.pt')
     model.load_state_dict(params)
 
@@ -112,13 +113,17 @@ def main(test_name):
         device = torch.device('cpu')
 
     batch: Batch = tree_map(lambda x: x.to(device=device), batch)
+    batch.reference_features.setup_block_mask(num_diffusion_samples=1)
+    batch.token_features.setup_block_mask()
 
     model = model.to(device=device)
     model.eval()
 
+    print('Running evoformer tests...')
     s_input, s_trunk, z_trunk, rel_feat = model.evoformer(batch)
     ttr.compare(s_trunk, 'evoformer/single')
     ttr.compare(z_trunk, 'evoformer/pair')
+    print('Evoformer tests complete.')
 
 
     def t2q(tensor):
@@ -143,6 +148,14 @@ def main(test_name):
         'aug_trans': ttr.load_all('diffusion/rand_aug/trans', processing=[indexing(0), to_device, to_float]),
     }
 
+    ttr.compare({
+        's_input': s_input,
+        's_trunk': s_trunk, 
+        'z_trunk': z_trunk,
+        'rel_feat': rel_feat,
+    }, 'quick_test')
+
+    print('Running diffusion tests...')
     with ttr.Chapter('diffusion'):
         diff_x = model.diffusion_sampler(model.diffusion_module,
                                 s_input, s_trunk, z_trunk, rel_feat, 
@@ -151,6 +164,7 @@ def main(test_name):
     diff_x = batch.reference_features.to_token_layout(diff_x)
 
     ttr.compare(diff_x, 'diffusion/final_positions', processing=[indexing(0), hotfix_roll_inv])
+    print('Diffusion tests complete.')
 
 def batch_test():
     test1 = 'lysozyme'
@@ -298,10 +312,10 @@ def inference():
 
 
 if __name__=='__main__':
-    with torch.no_grad():
-        inference()
-    # test_name = 'lysozyme'
-    # with torch.no_grad(), ttr.TensorTrace(f'data/tensortraces/{test_name}_trace', mode='read', framework='pytorch'):
-    #     main(test_name)
+    # with torch.no_grad():
+    #     inference()
+    test_name = 'lysozyme'
+    with torch.no_grad(), ttr.TensorTrace(f'/workspace/test/alphafold3-decoded/data/tensortraces/{test_name}_trace', mode='read', framework='pytorch'):
+        main(test_name)
     # with torch.no_grad():
     #     batch_test()

solutions/training/torchrun_training.py
@@ -157,11 +157,11 @@ def main():
 
     # sampler = build_sampler(train_ds)
     avail_workers = len(os.sched_getaffinity(0))
-    num_workers = 3
+    num_workers = 6
     print(f'[rank{rank}]: {num_workers}/{avail_workers} workers used')
     train_ds = torch.utils.data.Subset(train_ds, indices=range(rank, len(train_ds), world_size))
 
-    n_steps = 5002
+    n_steps = 2002
     n_steps_eval = 50
     n_steps_intermediate = 20
     
@@ -176,7 +176,7 @@ def main():
         def __getitem__(self, key):
             return self.base_dataset[key % len(self.base_dataset)]
 
-    # train_ds = EpochlessDataset(train_ds, n_steps)
+    train_ds = EpochlessDataset(train_ds, n_steps)
     train_dl = torch.utils.data.DataLoader(train_ds, num_workers=num_workers, batch_size=config.training_config.micro_batch_size, collate_fn=lambda x: collate_batch_drop_none(x, config))
 
     eval_ds = torch.utils.data.Subset(eval_ds, indices=range(rank, len(eval_ds), world_size))
@@ -275,6 +275,7 @@ def main():
                 wandb.run.log_artifact(cif_artifact)
 
         if (batch_idx+1)%500==0 and rank==0:
+            Path('checkpoints').mkdir(exist_ok=True)
             torch.save(model.state_dict(), f'checkpoints/step_{batch_idx:04d}.pt')
 
 